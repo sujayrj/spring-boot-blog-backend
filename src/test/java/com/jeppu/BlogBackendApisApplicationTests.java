f62a0cdbe15750cfdb8da625a64240cd31c0749d
Sujay Jeppu <sujayrj@gmail.com>
2022-12-20 14:05:23 +0530

    Initial Project structure

---
+++ src/test/java/com/jeppu/BlogBackendApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeppu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBackendApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
